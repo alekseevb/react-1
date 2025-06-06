6392467a3cc2e5edbf5484bff998177a762eee55
Алексей Алексеев <alexseev617@gmail.com>
2025-06-06 20:42:22 +0300

    commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import logo from '/logo-name.svg'
+
+export default function Header() {
+	const [now, setNow] = useState(new Date())
+
+	setInterval(() => setNow(new Date()), 1000)
+
+	return (
+		<header>
+			<img src={logo} alt='Result' />
+
+			<span>Время сейчас: {now.toLocaleTimeString()}</span>
+		</header>
+	)
+}
